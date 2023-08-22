9cd5802630ee525ae20f9e9561aa03b448e5f55d
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-08-22 16:24:46 -0400

    Use vectors/pairs instead of arrays

--- DoStandaloneCorrelatorCalculation.C
+++ DoStandaloneCorrelatorCalculation.C
@@ -23,36 +23,43 @@ R__LOAD_LIBRARY(/sphenix/user/danderson/install/lib/libsenergycorrelator.so)
 
 using namespace std;
 
-// global constants
-static const size_t NHistRange   = 2;
-static const size_t NAcceptRange = 2;
-static const size_t NEnergyCorrs = 2;
-
 
 
 void DoStandaloneCorrelatorCalculation() {
 
   // io parameters
-  const string inFile("../SCorrelatorJetTree/output/condor/final_merge/correlatorJetTree.pa200hijing500bkg010jet20run6_trks.d17m8y2023.root");
-  const string inTree[NEnergyCorrs]  = {
+  const vector<string> inFile = {
+    "../SCorrelatorJetTree/output/condor/final_merge/correlatorJetTree.pa200hijing500bkg010jet20run6_trks.d17m8y2023.root",
+    "../SCorrelatorJetTree/output/condor/final_merge/correlatorJetTree.pa200hijing500bkg010jet20run6_trks.d17m8y2023.root"
+  };
+  const vector<string> inTree = {
     "RecoJetTree",
     "TruthJetTree"
   };
-  const string outFile[NEnergyCorrs] = {
+  const vector<string> outFile = {
     "pa200hijing50khzrun6jet20.reco.d22m8y2023.root",
     "pa200hijing50khzrun6jet20.true.d22m8y2023.root"
   };
 
+  // module parameters
+  const vector<string> moduleName = {
+    "SRecoEnergyCorrelator",
+    "STrueEnergyCorrelator"
+  };
+  const vector<bool> isTruth = {
+    false,
+    true
+  };
+
   // correlator parameters
-  const uint32_t  nPointCorr             = 2;
-  const uint64_t  nBinsDr                = 75;
-  const double    binRangeDr[NHistRange] = {1e-5, 1.};
-  const bool      isTruth[NEnergyCorrs]  = {false, true};
+  const uint32_t             nPointCorr = 2;
+  const uint64_t             nBinsDr    = 75;
+  const pair<double, double> binRangeDr = {1e-5, 1.};
 
   // jet/cst parameters
-  const double etaJetRange[NAcceptRange] = {-1., 1.};
-  const double momCstRange[NAcceptRange] = {0.,  100.};
-  const double drCstRange[NAcceptRange]  = {0.,  5.};
+  const pair<double, double> etaJetRange = {-1., 1.};
+  const pair<double, double> momCstRange = {0.,  100.};
+  const pair<double, double> drCstRange  = {0.,  5.};
 
   // jet pT bins
   const vector<pair<double, double>> ptJetBins = {{5., 10.}, {10., 15.}, {15., 20.}, {20., 30.}, {30., 50.}};
@@ -64,27 +71,27 @@ void DoStandaloneCorrelatorCalculation() {
   const bool inBatch   = false;
 
   // do correlator calculation on reco jets
-  SEnergyCorrelator *recoCorrelator = new SEnergyCorrelator("SRecoEnergyCorrelator", isComplex, doDebug, inBatch);
+  SEnergyCorrelator *recoCorrelator = new SEnergyCorrelator(moduleName[0], isComplex, doDebug, inBatch);
   recoCorrelator -> SetVerbosity(verbosity);
-  recoCorrelator -> SetInputFile(inFile);
+  recoCorrelator -> SetInputFile(inFile[0]);
   recoCorrelator -> SetInputTree(inTree[0], isTruth[0]);
   recoCorrelator -> SetOutputFile(outFile[0]);
-  recoCorrelator -> SetJetParameters(ptJetBins, etaJetRange[0], etaJetRange[1]);
-  recoCorrelator -> SetConstituentParameters(momCstRange[0], momCstRange[1], drCstRange[0], drCstRange[1]);
-  recoCorrelator -> SetCorrelatorParameters(nPointCorr, nBinsDr, binRangeDr[0], binRangeDr[1]);
+  recoCorrelator -> SetJetParameters(ptJetBins, etaJetRange.first, etaJetRange.second);
+  recoCorrelator -> SetConstituentParameters(momCstRange.first, momCstRange.second, drCstRange.first, drCstRange.second);
+  recoCorrelator -> SetCorrelatorParameters(nPointCorr, nBinsDr, binRangeDr.first, binRangeDr.second);
   recoCorrelator -> Init();
   recoCorrelator -> Analyze();
   recoCorrelator -> End();
 
   // do correlator calculation on truth jets
-  SEnergyCorrelator *trueCorrelator = new SEnergyCorrelator("STrueEnergyCorrelator", isComplex, doDebug, inBatch);
+  SEnergyCorrelator *trueCorrelator = new SEnergyCorrelator(moduleName[1], isComplex, doDebug, inBatch);
   trueCorrelator -> SetVerbosity(verbosity);
-  trueCorrelator -> SetInputFile(inFile);
+  trueCorrelator -> SetInputFile(inFile[1]);
   trueCorrelator -> SetInputTree(inTree[1], isTruth[1]);
   trueCorrelator -> SetOutputFile(outFile[1]);
-  trueCorrelator -> SetJetParameters(ptJetBins, etaJetRange[0], etaJetRange[1]);
-  trueCorrelator -> SetConstituentParameters(momCstRange[0], momCstRange[1], drCstRange[0], drCstRange[1]);
-  trueCorrelator -> SetCorrelatorParameters(nPointCorr, nBinsDr, binRangeDr[0], binRangeDr[1]);
+  trueCorrelator -> SetJetParameters(ptJetBins, etaJetRange.first, etaJetRange.second);
+  trueCorrelator -> SetConstituentParameters(momCstRange.first, momCstRange.second, drCstRange.first, drCstRange.second);
+  trueCorrelator -> SetCorrelatorParameters(nPointCorr, nBinsDr, binRangeDr.first, binRangeDr.second);
   trueCorrelator -> Init();
   trueCorrelator -> Analyze();
   trueCorrelator -> End();
