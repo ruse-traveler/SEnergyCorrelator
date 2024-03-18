ef4bdb7cc2636f8b2e75d49490662f86d9196929
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-03-18 14:21:04 -0400

    Set up method to translate between old/new inputs

--- src/SEnergyCorrelatorInput.h
+++ src/SEnergyCorrelatorInput.h
@@ -23,18 +23,18 @@ namespace SColdQcdCorrelatorAnalysis {
   struct SEnergyCorrelatorInput {
 
     // event level info
-    Types::RecoInfo m_reco;
-    Types::GenInfo  m_gen;
+    Types::RecoInfo reco;
+    Types::GenInfo  gen;
 
     // jet and constituent info
-    vector<Types::JetInfo>         m_jets;
-    vector<vector<Types::CstInfo>> m_csts; 
+    vector<Types::JetInfo>         jets;
+    vector<vector<Types::CstInfo>> csts; 
 
     void Reset() {
-      m_reco.Reset();
-      m_gen.Reset();
-      m_jets.clear();
-      m_csts.clear();
+      reco.Reset();
+      gen.Reset();
+      jets.clear();
+      csts.clear();
       return;
     }  // end 'Reset()'
 
@@ -82,6 +82,8 @@ namespace SColdQcdCorrelatorAnalysis {
     vector<vector<double>>* cstEta     = NULL;
     vector<vector<double>>* cstPhi     = NULL;
 
+
+
     void Reset() {
       evtNumChrgPars = numeric_limits<int>::min();
       evtSumPar      = numeric_limits<double>::min();
@@ -115,6 +117,8 @@ namespace SColdQcdCorrelatorAnalysis {
       cstPhi         = NULL;
       return;
     }  // end 'Reset()'
+
+
 
     void SetChainAddresses(TChain* chain, const bool isInTreeTruth = true) {
       if (isInTreeTruth) {
@@ -154,6 +158,91 @@ namespace SColdQcdCorrelatorAnalysis {
       chain -> SetBranchAddress("CstPhi",     &cstPhi);
       return;
     }  // end 'SetChainAddresses(TChain*)'
+
+
+
+    void SetCorrelatorInput(SEnergyCorrelatorInput& input, const bool isInTreeTruth = true, optional<bool> isEmbed = nullopt) {
+
+      // make sure container is empty
+      input.Reset();
+
+      // set reco event-level variables
+      input.reco.SetNTrks( evtNumTrks );
+      input.reco.SetESumEMCal( evtSumECal );
+      input.reco.SetESumIHCal( evtSumHCal );
+      input.reco.SetESumOHCal( evtSumHCal );
+      input.reco.SetVX( evtVtxX );
+      input.reco.SetVY( evtVtxY );
+      input.reco.SetVZ( evtVtxZ );
+      input.reco.SetVR( hypot(evtVtxX, evtVtxY) );
+
+      // set generator event-level variables
+      if (isInTreeTruth) {
+
+        // set parton variables
+        pair<Types::ParInfo, Types::ParInfo> partons;
+        partons.first.SetBarcode( partonID.first );
+        partons.first.SetPX( partonMomX.first );
+        partons.first.SetPY( partonMomY.first );
+        partons.first.SetPZ( partonMomZ.first );
+        partons.first.SetPT( hypot(partonMomX.first, partonMomY.first) );
+        partons.first.SetVX( evtVtxX );
+        partons.first.SetVY( evtVtxY );
+        partons.first.SetVZ( evtVtxZ );
+        partons.first.SetVR( hypot(evtVtxX, evtVtxY) );
+        partons.second.SetBarcode( partonID.second );
+        partons.second.SetPX( partonMomX.second );
+        partons.second.SetPY( partonMomY.second );
+        partons.second.SetPZ( partonMomZ.second );
+        partons.second.SetPT( hypot(partonMomX.second, partonMomY.second) );
+        partons.second.SetVX( evtVtxX );
+        partons.second.SetVY( evtVtxY );
+        partons.second.SetVZ( evtVtxZ );
+        partons.second.SetVR( hypot(evtVtxX, evtVtxY) );
+
+        // set event variables
+        input.gen.SetESumChrg( evtSumPar );
+        input.gen.SetESumNeu( evtSumPar );
+        input.gen.SetPartons(partons);
+        if (isEmbed.has_value()) {
+          input.gen.SetIsEmbed( isEmbed.value() );
+        }
+      }
+
+      // set jet variables
+      input.jets.resize( jetNumCst -> size() );
+      for (size_t iJet = 0; iJet < jetNumCst -> size(); iJet++) {
+        input.jets.at(iJet).SetJetID( jetID -> at(iJet) );
+        input.jets.at(iJet).SetNCsts( jetNumCst -> at(iJet) );
+        input.jets.at(iJet).SetEne( jetEnergy -> at(iJet) );
+        input.jets.at(iJet).SetPT( jetPt -> at(iJet) );
+        input.jets.at(iJet).SetEta( jetEta -> at(iJet) );
+        input.jets.at(iJet).SetPhi( jetPhi -> at(iJet) );
+        input.jets.at(iJet).SetArea( jetArea -> at(iJet) );
+      }  // end jet loop
+
+      // set cst variables
+      input.csts.resize( jetNumCst -> size() );
+      for (size_t iJet = 0; iJet < jetNumCst -> size(); iJet++) {
+        input.csts.at(iJet).resize( (cstZ -> at(iJet)).size() );
+        for (size_t iCst = 0; iCst < (cstZ -> at(iJet)).size(); iCst++) {
+          input.csts.at(iJet).at(iCst).SetJetID( jetID -> at(iJet) );
+          input.csts.at(iJet).at(iCst).SetZ( (cstZ -> at(iJet)).at(iCst) );
+          input.csts.at(iJet).at(iCst).SetDR( (cstDr -> at(iJet)).at(iCst) );
+          input.csts.at(iJet).at(iCst).SetPT( (cstPt -> at(iJet)).at(iCst) );
+          input.csts.at(iJet).at(iCst).SetEta( (cstEta -> at(iJet)).at(iCst) );
+          input.csts.at(iJet).at(iCst).SetPhi( (cstPhi -> at(iJet)).at(iCst) );
+          if (isInTreeTruth) {
+            input.csts.at(iJet).at(iCst).SetEmbedID( (cstEmbedID -> at(iJet)).at(iCst) );
+            input.csts.at(iJet).at(iCst).SetCstID( (cstID -> at(iJet)).at(iCst) );
+          } else {
+            input.csts.at(iJet).at(iCst).SetCstID( (cstMatchID -> at(iJet)).at(iCst) );
+          }
+        }  // end c st loop
+      }  // end jet loop
+      return;
+
+    }  // end 'SetCorrelatorInput(SEnergyCorrelatorInput&)'
 
   };  // end SEnergyCorrelatorLegacyInput
 
